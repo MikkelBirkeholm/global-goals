e1dd3755dc7cbb55e358d13a98fbf320fcfedaa5
Birkeholm <mikkel.birkeholm@gmail.com>
2022-12-10 13:22:16 +0100

    Småting

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,11 @@ const goalsGrid = document.querySelector('#goal-grid')
 
 fetch('contentrain/Goals/Goals.json').then(response => response.json())
     .then(data => {
+        for (let item of data) {
+            console.log(item.cover)
+            // goalsGrid.innerHTML += `<li style="background-image:url('${data[0].cover}');"></li>`
+        }
 
-        goalsGrid.innerHTML += `<li>${data[0].title}<br /> ${data[0].description}</li>`
-
-        console.log(data)
     })
 
 
