cf0e7405116532b6e013a1ddf9cd8e7850baaabb
Birkeholm <mikkel.birkeholm@gmail.com>
2022-12-12 10:28:27 +0100

    Billeder mm

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,7 @@ const goalsGrid = document.querySelector('#goal-grid')
 fetch('contentrain/Goals/Goals.json').then(response => response.json())
     .then(data => {
         for (let item of data) {
-            console.log(item.cover)
-            // goalsGrid.innerHTML += `<li style="background-image:url('${data[0].cover}');"></li>`
         }
-
     })
 
 
