2d22f4d6a569c39e6f50d08df9b37eb365415b2c
Birkeholm <mikkel.birkeholm@gmail.com>
2022-12-12 12:09:41 +0100

    Dezignz

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 const goalsGrid = document.querySelector('#goal-grid')
+const dialog = document.querySelector('#dialog')
 
 fetch('contentrain/Goals/Goals.json').then(response => response.json())
     .then(data => {
         for (let item of data) {
+            goalsGrid.innerHTML += `<li><img src="${item.cover}" alt="" /></li>`
         }
+        goalsGrid.innerHTML += `<li id="no-goal"></li>`
     })
 
 
