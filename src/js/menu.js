95b93d307402f7284f218cb043b99a98ec9f639f
simo994q <simonkn1@hotmail.com>
2022-12-14 09:22:18 +0100

    jss

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,11 +1,6 @@
 const dropdown = document.querySelector('.dropdown');
 const menu = document.querySelector('#menu');
-const dropdowna = document.querySelectorAll('.dropdown-a');
 
-
-menu.addEventListener('click', () => {
-    dropdown.classList.toggle('detvejegik')
-})
-dropdowna.addEventListener('click', () => {
+menu.addEventListener('click', function () {
     dropdown.classList.toggle('detvejegik')
 })
